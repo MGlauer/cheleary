e1e12bd1416f29b8ab36d5d700d762e9cf0fa8a6
MGlauer <martinglauer89@gmail.com>
2020-06-15 16:36:33 +0200

    Use pure LSTM

--- classifier.py
+++ classifier.py
@@ -6,13 +6,12 @@ from encode import encode_smiles, input_lenth
 
 def create_model(input_shape):
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Input(shape=(None, input_lenth), ragged=True))
-    model.add(tf.keras.layers.LSTM(1000, activation='relu', input_shape=input_shape, name="forward"))
-    model.add(tf.keras.layers.Dense(10000, activation="relu"))
-    model.add(tf.keras.layers.Dense(5000, activation="relu"))
-    model.add(tf.keras.layers.Dense(1024, activation="sigmoid"))
-    model.compile(optimizer='adam', loss="binary_crossentropy",
-  metrics=["accuracy"])
+    #model.add(tf.keras.layers.Input(shape=(None, input_lenth), ragged=True))
+    model.add(tf.keras.layers.LSTM(1024, activation='tanh', recurrent_activation='sigmoid', recurrent_dropout=0, unroll=False, input_shape=input_shape, name="forward"))
+    #model.add(tf.keras.layers.Dense(10000, activation="relu"))
+    #model.add(tf.keras.layers.Dense(5000, activation="tanh"))
+    #model.add(tf.keras.layers.Dense(1024, activation="tanh"))
+    model.compile(optimizer='adam', loss="mse", metrics=["accuracy"])
     return model
 
 def generate():
@@ -29,11 +28,12 @@ model = create_model((None, input_lenth))
 
 tf.keras.utils.plot_model(model, show_shapes=True, to_file='reconstruct_lstm_autoencoder.png')
 # fit model
-
-x, y = zip(*generate())
-x = tf.ragged.constant(x)
-y = tf.convert_to_tensor(y)
+#L = generate()
+xs, ys = zip(*generate())
+x = tf.ragged.constant(xs)
+y = tf.convert_to_tensor(ys)
 #ds = tf.data.Dataset.from_tensor_slices((x, y))
+
 
 model.fit(x, y, epochs=300, use_multiprocessing=True)
 model.save("out")
