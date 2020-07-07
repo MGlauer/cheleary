e94eaee1314daf4d0e372c8281afe5b4274ef765
MGlauer <martinglauer89@gmail.com>
2020-07-07 11:48:42 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='cheleary',
+    version='',
+    packages=['src', 'src.cheleary'],
+    url='https://github.com/MGlauer/cheleary',
+    license='',
+    install_requires=[
+        'tensorflow',
+        'numpy',
+        'pysmiles',
+        'pandas'
+    ],
+    author='glauer',
+    author_email='',
+    description=''
+)
