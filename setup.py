36819203293a6a49d58dbc95ee9a263db9b42ee4
MGlauer <martinglauer89@gmail.com>
2020-07-08 22:41:56 +0200

    Add entry points for setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 from setuptools import find_packages
+from glob import glob
+from os.path import basename
+from os.path import splitext
 
 setup(
     name="cheleary",
     version="0.0.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
+    zip_safe=False,
     url="https://github.com/MGlauer/cheleary",
     license="",
     install_requires=["tensorflow", "numpy", "pysmiles", "pandas"],
@@ -16,4 +22,5 @@ setup(
                  different kinds of encodings and network structures based on `keras` and \
                  `tensorflow`. The main focus are learning tasks around `CHEBI` - an \
                  ontology about chemicals",
+    entry_points={"console_scripts": ["cheleary = cheleary.cli:cli"]},
 )
