34018a2989e965edb8388cd3bf88dba7cd72b1a1
MGlauer <martinglauer89@gmail.com>
2020-10-16 11:49:19 +0200

    Fix possible circular import with tensorflow

--- src/cheleary/__main__.py
+++ src/cheleary/__main__.py
@@ -1,4 +1,5 @@
-from cheleary.cli import cli
-
 if __name__ == "__main__":
+    # This is here because Tensorflow is trying to be smart sometimes and breaks
+    from cheleary.cli import cli
+
     cli()
