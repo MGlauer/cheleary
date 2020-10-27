61f392b7f02ab1a6eacb692c447dc9b5c741c3ed
MGlauer <martinglauer89@gmail.com>
2020-10-27 16:46:01 +0100

    Implement LogitEncoder

--- src/cheleary/encode.py
+++ src/cheleary/encode.py
@@ -1646,44 +1646,26 @@ class FingerprintEncoder(Encoder):
                 return list(map(bool, Chem.RDKFingerprint(mol, fpSize=1024)))
 
 
-class MorganFingerprintEncoder(Encoder):
-    _ID = "MFE"
+class ReadyFingerprintEncoder(Encoder):
+    _ID = "RFE"
 
     def run(self, input):
+        return np.asarray(tuple(map(int, input[3:])))
+
+
+class ChebiLogitEncode(Encoder):
+    _ID = "CLE"
+
+    d = {}
+    max_index = -1
+
+    def run(self, input):
+        cls = input[0]
+        n = np.zeros(100)
         try:
-            import rdkit
-            from rdkit import Chem
-            from rdkit.Chem import rdMolDescriptors
-        except Exception as e:
-            raise e
-        else:
-            smiles = input[1]
-            radi = 2
-            binary = np.zeros((2048 * (radi)), int)
-            formula = np.zeros((2048), int)
-            mol = Chem.MolFromSmiles(smiles)
-            if mol:
-                mol = Chem.AddHs(mol)
-                mol_bi = {}
-                for r in range(radi + 1):
-                    mol_fp = rdMolDescriptors.GetMorganFingerprintAsBitVect(
-                        mol, radius=r, bitInfo=mol_bi, nBits=2048
-                    )
-                    mol_bi_QC = []
-                    for i in mol_fp.GetOnBits():
-                        num_ = len(mol_bi[i])
-                        for j in range(num_):
-                            if mol_bi[i][j][1] == r:
-                                mol_bi_QC.append(i)
-                                break
-
-                    if r == 0:
-                        for i in mol_bi_QC:
-                            formula[i] = len([k for k in mol_bi[i] if k[1] == 0])
-                    else:
-                        for i in mol_bi_QC:
-                            binary[(2048 * (r - 1)) + i] = len(
-                                [k for k in mol_bi[i] if k[1] == r]
-                            )
-
-                return np.concatenate([formula, binary])
+            idx = self.d[cls]
+        except KeyError:
+            self.max_index += 1
+            self.d[cls] = idx = self.max_index
+        n[idx] = 1
+        return n
